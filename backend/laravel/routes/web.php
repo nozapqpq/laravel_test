6fc25418117f90a896bbf57224a76e9fe3bb2a2f
Nozaki <moyashimizu0831@gmail.com>
2022-04-06 23:14:38 +0900

    #3 netkeiba処理の追加(先にkaisai処理が必要だった)

--- backend/laravel/routes/web.php
+++ backend/laravel/routes/web.php
@@ -16,3 +16,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('netkeiba','App\Http\Controllers\ScrapingController@netkeiba');
+
+/*
+ * ***パッケージ、composer関連の情報***
+ * 最初に下記をcomposerに取り込む必要がある。取り込むとcomposer.lockが更新される
+ * composer require rap2hpoutre/fast-excel
+ * composer require weidner/goutte：古めのlaravelしか使えなさそう
+ * composer require fabpot/goutte
+ * 
+ * composerのリセットはcomposer install
+ * インストールされているパッケージの確認はcomposer show -i
+ * 
+ * キャッシュのクリア
+ * php artisan config:clear
+ * 
+ * データベースはこう
+ * まず.envのDB_HOST,ユーザ名パスワードなど設定(このファイルの更新はgitにコミットしない)
+ * 次にmysqlにデータベースを作る
+ * php artisan make:migration create_hoge_table
+ * データベースのマイグレーションの実行(未処理のものすべてを実行)
+ * php artisan migrate
+
+ * できたら次はControllerの作成
+ * php artisan make:controller HOGEController
+ */
