d172652a6117e4e68d4e8b498d325ea0b27fecc7
Nozaki <moyashimizu0831@gmail.com>
2022-05-20 00:18:08 +0900

    refs #7 abstructへの概要説明追加

--- backend/laravel/resources/views/page_abstruct.blade.php
+++ backend/laravel/resources/views/page_abstruct.blade.php
@@ -10,7 +10,15 @@
 <body>
     <div class="abstruct">
         <h2>Laravelとは</h2>
-        <p>MVCのWebアプリケーション開発用のPHPで書かれたWebアプリケーションフレームワークです</p>
+          <p>MVCのWebアプリケーション開発用のPHPで書かれたWebアプリケーションフレームワークです</p><br>
+          <a href="https://ja.wikipedia.org/wiki/Laravel">詳しくはこちら</a><br>
+        <h2>メリット、デメリット</h2>
+          メリットはよく使われていて便利なこと<br>
+          デメリットは重いこと<br>
+          <a href="https://city3939.com/laravel-merit-and-demerit/">こちら</a><br>
+        <h2>他のPHPフレームワーク(CakePHP)との違い</h2><br>
+          CakePHPは速く、Laravelは便利です<br>
+          <a href="https://qiita.com/testMenta339/items/6017417e536af0adcd01">詳しくはこちら</a><br>
 
         <h2>Laravelの機能について</h2>
         <a href="{{ url('page_routing') }}">ルーティング</a><br>
@@ -21,6 +29,8 @@
         <a href="{{ url('page_cert') }}">認証</a><br>
         <a href="{{ url('page_test') }}">ユニットテスト</a><br>
         <a href="{{ url('page_artisan') }}">Artisanコンソール</a><br>
+
+        <h2>使っていてやりやすかったこと、やりづらかったこと</h2>
         
     </div>
 </body>
