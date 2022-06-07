6b363f7f02e842d59c02e09d2f113cc1639e0c8f
Nozaki <moyashimizu0831@gmail.com>
2022-06-07 22:55:50 +0900

    refs #7 blade.php追加

---
+++ backend/laravel/resources/views/query_sample.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>リクエスト処理</title>
+</head>
+
+<body>
+  <h2>送信されてきたもの</h2>
+
+  {{ $sample }}<br>
+
+
+</body>
+</html>
